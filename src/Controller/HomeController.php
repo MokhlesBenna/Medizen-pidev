fde2622b31ea6449456aa3dedfee669ac115ad2e
Rouba21 <rouba.souidi@esprit.tn>
2024-02-22 00:45:02 +0100

    user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,10 +8,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'app_index_front')]
     public function index(): Response
     {
+        
+        //dd($this->getUser()->getRoles()[0]);
+        if ($this->getUser()->getRoles()[0]=="ROLE_USER")
         return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+        else if ($this->getUser()->getRoles()[0]=="ROLE_ADMIN"){
+            return $this->render('home/index_admin.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+        }
+    }
+     #[Route('/admin', name: 'app_index_admin')]
+    public function index_admin(): Response
+    {
+        return $this->render('home/index_admin.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
